03d945645f7ddd1ea9c02a8d5bf74a9b988d44d1
diego <diegoalonsonm@gmail.com>
2024-04-19 11:24:11 -0600

    cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://cash-controller-client.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://cash-controller-client.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
